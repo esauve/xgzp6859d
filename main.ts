c46295e446c496da40cc691be11ae417dab44a1c
esauve <sauvee@hotmail.com>
2026-03-28 12:41:39 -0400

    Fix formatting issues and add missing newline

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 //% color="#0078D7" weight=100 icon="\uf072"
+//% block="XGZP6859D"
 namespace XGZP6859D {
 
     const ADDR = 0x6D
@@ -7,12 +8,14 @@ namespace XGZP6859D {
     const K = 512
 
     //% block="lire capteur XGZP6859D"
+    //% weight=100
     export function lireCapteur(): void {
         pins.i2cWriteNumber(ADDR, (REG_CMD << 8) | CMD_MESURE, NumberFormat.UInt16BE)
         basic.pause(5)
     }
 
     //% block="pression en kPa"
+    //% weight=90
     export function pressionKPa(): number {
         pins.i2cWriteNumber(ADDR, 0x04, NumberFormat.UInt8BE)
         let buf = pins.i2cReadBuffer(ADDR, 3)
@@ -22,6 +25,7 @@ namespace XGZP6859D {
     }
 
     //% block="température en °C"
+    //% weight=80
     export function temperature(): number {
         pins.i2cWriteNumber(ADDR, 0x07, NumberFormat.UInt8BE)
         let buf = pins.i2cReadBuffer(ADDR, 2)
